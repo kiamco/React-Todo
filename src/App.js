a6746bb6ad2a61bb52b403f6961f50c25f9d5569
kiamco <kiamcoo@gmail.com>
2019-09-03 22:04:43 -0700

    it begins

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react';
+import ListItems from './components/TodoComponents/TodoList.js'
+
+const todos = [{
+  date: Date.now(),
+  title: 'asd',
+  description: 'asd',
+  deadline: 'asd',
+}]
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
+  constructor() {
+    super();
+    this.state = {
+      todos
+    };
+  }
+
+  // this.setState({
+  //   todos: this.state.todos.map(el =>)
+  // })
   // design `App` to be the parent component of your application.
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className='App'>
+        <div classname='todo-container'>
+          <h2>Todo List</h2>
+          {/* render list */}
+          <ListItems item={this.state.todos}/>
+          {/* render form and but
+          tons */}
+
+        </div>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
+
+
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+
+import React from 'react';
+
+const ListItem = props => {
+    return(
+    <div className='list-items'>
+        <h4>{props.item.title}</h4>
+        <p>{props.item.description}</p>
+        <p>{props.item.deadline}</p>
+        <p>{props.item.date}</p>
+    </div>
+    )
+}
+
+export default ListItem;
